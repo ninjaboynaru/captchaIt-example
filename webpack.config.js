94fcc2d2e60a04cfc9ead68b9935208692bc76de
ninjaboynaru <thiagoCord755@gmail.com>
2018-06-14 16:50:46 -0400

    Webpack mode

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,23 @@
 const path = require('path');
 
+let mode;
+if(process.env.NODE_ENV === 'production') {
+	console.log('Building for production');
+	mode = 'production';
+}
+else {
+	console.log('Building for development');
+	mode = 'development';
+}
+
 module.exports = {
+	mode,
 	entry: path.join(__dirname, '/source/js/index.js'),
+	resolve: {
+		alias: {
+			'vue$': 'vue/dist/vue.esm.js'
+		}
+	},
 	output: {
 		path: path.join(__dirname, '/docs/js'),
 		filename: 'index.js'
